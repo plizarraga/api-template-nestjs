71e1bfcb22f421ac734b4dbdd9a0988b7fbfdf3d
Pedro Lizarraga <plizarraga86@gmail.com>
2023-10-14 22:29:16 -0700

    Added CORS, morgan, setup env file (.development.env)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import * as morgan from 'morgan';
 import { AppModule } from './app.module';
+import { CORS } from './constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.use(morgan('dev'));
+  app.enableCors(CORS);
+  app.setGlobalPrefix('api');
+
+  const configService = app.get(ConfigService);
+
+  await app.listen(configService.get('PORT'));
+  console.log(`Server is running on port ${configService.get('PORT')}`);
 }
 bootstrap();
